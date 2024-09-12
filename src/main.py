2d409c82e1cf3210886731285d0476b758fddafc
qitianshi <65122416+qitianshi@users.noreply.github.com>
2024-09-12 15:53:43 +0800

    Add main.py with bot entry point

---
+++ src/main.py
@@ -0,0 +1,37 @@
+"""Entry point for Duty Rooster."""
+
+# Copyright 2024 Qi Tianshi. All rights reserved.
+
+
+import os
+import logging
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+
+# Logging configs.
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Onboarding for new users."""
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Cock-a-doodle-doo! I'm Duty Rooster, the duty roster that isn't"
+            + " bird-brained. Something's hatching soon, so watch this coop!"
+    )
+
+
+if __name__ == "__main__":
+
+    app = ApplicationBuilder().token(os.getenv("TEST_BOT_TOKEN")).build()
+
+    start_handler = CommandHandler('start', start)
+    app.add_handler(start_handler)
+
+    app.run_polling()
